1ad9e9c6984a885ca91552bfd3ad3fa725dd66bc
Mohammad Aman Malik <amankgn108@gmail.com>
2025-07-08 10:50:29 +0530

    Star-Rating

---
+++ Rating/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import StarRating from './StarRating'
+
+function App() {
+
+  return (
+    <>
+      <StarRating/>
+    </>
+  )
+}
+
+export default App
---
+++ Rating/src/StarRating.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const StarRating = ({ noOfStars = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(currentIndex) {
+    setRating(currentIndex);
+  }
+  function handleMouseEnter(currentIndex) {
+    setHover(currentIndex);
+  }
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="starRating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
